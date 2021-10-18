f930320aa278c9be2af87eccec07b7d1cc278ff5
joel <TeslaTeslon@fastweb.com>
2021-10-17 22:30:06 -0500

    second_commit

--- Go-Api-Rest/main.go
+++ Go-Api-Rest/main.go
@@ -2,36 +2,35 @@ package main
 
 import (
 	"fmt"
-	"net/http"
 	"log"
+	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
-type task struct{
-	ID int `json:ID`
-	Name string `json:Name`
+type task struct {
+	ID      int    `json:ID`
+	Name    string `json:Name`
 	Content string `json:Content`
 }
 type allTasks []task
 
 var tasks = allTasks{
 	{
-		ID:1,
-		Name:"Task One",
-		Content:"Some content",
-	
+		ID:      1,
+		Name:    "Task One",
+		Content: "Some content",
 	},
-		
 }
-func indexRoute(w http.ResponseWriter,r *http.Request){
-	fmt.Fprintf(w,"wecolme to my api")
+
+func indexRoute(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "wecolme to my apidddsddd")
 
 }
 
-func main(){
-	
-	router:=mux.NewRouter().StrictSlash(true)
-	router.HandleFunc("/",indexRoute)
-	log.Fatal(http.ListenAndServe(":3000",router))
+func main() {
+
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", indexRoute)
+	log.Fatal(http.ListenAndServe(":3000", router))
 }
